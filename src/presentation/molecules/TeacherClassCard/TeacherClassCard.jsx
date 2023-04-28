0a1a6e8fbf7990f451bf0ba5e33a825a80698d59
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2023-04-28 17:26:51 +0200

    teacher classes WiP

--- src/presentation/molecules/TeacherClassCard/TeacherClassCard.jsx
+++ src/presentation/molecules/TeacherClassCard/TeacherClassCard.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState, useContext } from "react"
+import { UserContext } from "../../../services/context/user"
+
 import Trash from "../../../medias/icons/trash.png"
 import Pencil from "../../../medias/icons/pencil.png"
 import Eye from "../../../medias/icons/eye.png"
 
+import { getTeacherClasseAvailabilities } from "../../../services/api/Classes"
+
 import "./teacher-class-card.css"
 
-const TeacherClassCard = ({ classInfos, isSelected }) => {
+const TeacherClassCard = ({ classInfos, isSelected, onClick, index }) => {
+    const { userToken } = useContext(UserContext)
+    const fetchClassAvailabilities = async () => {
+        const fetchedAvailabilities = await getTeacherClasseAvailabilities(
+            userToken,
+            classInfos.id
+        )
+        if (fetchedAvailabilities) {
+            setAvailabilities(fetchedAvailabilities)
+        }
+    }
+    const [availabilities, setAvailabilities] = useState(null)
+    useEffect(() => {
+        fetchClassAvailabilities()
+    }, [])
     return (
         <div
             className={`teacher-class-card ${
@@ -13,10 +32,15 @@ const TeacherClassCard = ({ classInfos, isSelected }) => {
         >
             <p className="teacher-class-card-class-name">{classInfos.name}</p>
             <p className="teacher-class-card-class-students">
-                {classInfos.number} élèves
+                {classInfos.size} élèves
             </p>
             <div className="teacher-class-card-icons">
-                <img className="teacher-class-card-icon" src={Eye} alt="eye" />
+                <img
+                    onClick={() => onClick(index)}
+                    className="teacher-class-card-icon"
+                    src={Eye}
+                    alt="eye"
+                />
                 <img
                     className="teacher-class-card-icon"
                     src={Pencil}
--- src/presentation/pages/TeacherClasses.jsx
+++ src/presentation/pages/TeacherClasses.jsx
@@ -1,18 +1,59 @@
+import { useEffect, useContext, useState } from "react"
+import { getTeacherClasses } from "../../services/api/Profile"
+import { UserContext } from "../../services/context/user"
+
 import MainLayout from "../layouts/MainLayout/MainLayout"
 import Title from "../atoms/Title/Title"
 import Button from "../atoms/Button/Button"
 import SeparatorLine from "../atoms/Line/SeparatorLine"
 import Logo from "../atoms/Logo/Logo"
 import Paragraph from "../atoms/Paragraph/Paragraph"
+import TeacherClassCard from "../molecules/TeacherClassCard/TeacherClassCard"
 
 const TeacherClasses = () => {
+    const { userToken } = useContext(UserContext)
+    const [currentClasses, setCurrentClasses] = useState([])
+    const fetchClasses = async () => {
+        if (userToken) {
+            const fetchedClasses = await getTeacherClasses(userToken)
+            if (fetchedClasses) {
+                const test = fetchedClasses.map((fetchedClasse) => {
+                    return { ...fetchedClasse, isSelected: false }
+                })
+                setCurrentClasses(test)
+            }
+        }
+    }
+    useEffect(() => {
+        fetchClasses()
+    }, [userToken])
+
+    const handleEyeClick = (index) => {
+        const updateClasses = [...currentClasses]
+        updateClasses.forEach((updateClasse) => {
+            updateClasse.isSelected = false
+        })
+        updateClasses[index].isSelected = true
+        setCurrentClasses(updateClasses)
+    }
+
     return (
         <MainLayout>
             <Logo />
             <Title title="Mes classes" />
             <Paragraph content="Renseignez ici vos classes" />
             <SeparatorLine />
-            {/* bloc de classes */}
+            {currentClasses?.map((currentClass, i) => {
+                return (
+                    <TeacherClassCard
+                        isSelected={currentClass.isSelected}
+                        key={currentClass.name}
+                        classInfos={currentClass}
+                        onClick={handleEyeClick}
+                        index={i}
+                    />
+                )
+            })}
             <Button color="blue" content="Ajouter une classe" />
         </MainLayout>
     )

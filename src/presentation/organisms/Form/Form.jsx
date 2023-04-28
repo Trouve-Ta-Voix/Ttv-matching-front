299fbe27f183c596179a1d4c7ee5bd38bdd8a9b0
FloMenus <florent.mns@gmail.com>
2023-04-28 14:19:02 +0200

    chore: fixed git mistake with Form compo

--- src/presentation/organisms/Form/Form.jsx
+++ src/presentation/organisms/Form/Form.jsx
@@ -1,8 +1,32 @@
-import React from "react"
-import formik from "formik"
+import TextInput from "../../molecules/TextInput/TextInput"
+import Button from "../../atoms/Button/Button"
 
-const Form = () => {
-    return <div>Form</div>
+const Form = ({ inputs, onSubmit, button, children }) => {
+    return (
+        <form onSubmit={onSubmit}>
+            {children}
+            {inputs.map(
+                ({ value, name, label, type, placeholder, onChange }) => {
+                    return (
+                        <TextInput
+                            key={name}
+                            label={label}
+                            value={value}
+                            type={type}
+                            placeholder={placeholder}
+                            name={name}
+                            onChange={onChange}
+                        />
+                    )
+                }
+            )}
+            <Button
+                type="submit"
+                color={button.color}
+                content={button.content}
+            />
+        </form>
+    )
 }
 
 export default Form
